cf084835b41498b9cdf6c04acecbca0889914e4d
Piero-marino <67409264+Piero-marino@users.noreply.github.com>
2020-09-06 23:09:02 -0300

    rat
    ing

--- js/product-info.js
+++ js/product-info.js
@@ -26,6 +26,22 @@ function showImagesGallery(array) {
   }
 }
 
+/* function showRating(rating) {
+  let ratingId = document.getElementById("commentRating");
+  content = "";
+  moreContent = "";
+  for (i = 0; i < rating; i++) {
+    content += '<span class="fa fa-star checked"></span>';
+  }
+  for (i = 5; i > rating; i--) {
+    moreContent += '<span class="fa fa-star"></span>';
+  }
+  ratingId.innerHTML += content;
+  ratingId.innerHTML += moreContent;
+  console.log("no funca esto lpm");
+}
+*/
+
 document.addEventListener("DOMContentLoaded", function (e) {
   alert.style.display = "none";
   fetch(PRODUCT_INFO_URL)
@@ -42,5 +58,37 @@ document.addEventListener("DOMContentLoaded", function (e) {
       productDescription.innerHTML = `${product.description}`;
       productSoldCount.innerHTML = `${product.soldCount} vendidos`;
       showImagesGallery(product.images);
+    });
+  fetch(PRODUCT_INFO_COMMENTS_URL)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (product) {
+      let content = "";
+      console.log(product);
+      for (i = 0; i < product.length; i++) {
+        let currentComment = product[i];
+        let currentRating = product[i].score;
+        let htmlcontentxD = "<strong>hola</strong>";
+        let estrellaChecked = '<span class="fa fa-star checked"></span>';
+        let estrella = '<span class="fa fa-star"></span>';
+        content += `<div class="list-group-item list-group-item-action">
+        <div class="row">
+            <div class="col">
+                <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1" id="username">${currentComment.user}</h4>
+                    <small class="text-muted" id="date">${
+                      currentComment.dateTime
+                    }</small>
+                </div>
+                <p class="mb-1" id="review ">${currentComment.description}</p>
+                <div class="productRating" id="commentRating">Rating: ${estrellaChecked.repeat(
+                  currentRating
+                )}${estrella.repeat(5 - currentRating)}</div>
+            </div>
+        </div>
+    </div>`;
+      }
+      document.getElementById("comments").innerHTML += content;
     });
 });
